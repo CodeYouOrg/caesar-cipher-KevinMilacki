02f045448a65cf929a579bfb1c42c9be83212e68
Kevin Milacki <kevin.milacki@gmail.com>
2024-08-14 10:35:59 -0400

    Completed Ceasar Cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,21 @@
 # add your code here
+
+
+def cipher(text):
+    result = ""
+    
+    for char in text:
+        if char.isupper():
+            new_char = chr((ord(char) + 5 - 65) % 26 + 65)
+            result += new_char
+        elif char.islower():
+            new_char = chr((ord(char) + 5 - 97) % 26 + 97)
+            result += new_char
+        else:
+            result += char
+    return result
+
+text = input("What do you want to encode?:")
+
+print(cipher(text))
+
